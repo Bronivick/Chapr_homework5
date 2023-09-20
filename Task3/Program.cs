2b3d742f37521a8cf6651cfe4df95555f940dfa5
Bronivick <mister.galiakhmetov2016@yandex.ru>
2023-09-21 01:00:44 +0300

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿double[] array = new double[4];
+
+for (int i = 0; i < array.Length; i++)
+{
+    double number = new Random().Next(10, 20) + new Random().NextDouble();
+    array[i] = Math.Round(number, 2);
+}
+
+Console.WriteLine("[" + String.Join(", ", array) + "]");
+double max = 0;
+double min = array[0];
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max) 
+        max = array[i];
+    else if (array[i] < min)
+        min = array[i];
+    
+}
+
+Console.WriteLine($"Разница между max и min = {max - min}");
