07881a63c1cffcd05d3c0e46154e2e7291784e2d
Bronivick <mister.galiakhmetov2016@yandex.ru>
2023-09-21 00:12:53 +0300

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = new int[4];
+
+int dotnEven = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(10, 20); 
+}
+
+Console.WriteLine("[" + String.Join(", ", array) + "]");
+for (int j = 0; j< array.Length; j++)
+    if (array[j] % 2 == 1)
+
+        dotnEven=array[0]+array[2];
+
+Console.WriteLine($"Сумма нечетных позиций = {dotnEven}");
