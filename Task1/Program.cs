21ac8b1329564c01a8ecd9588d93d2dea7ee9a86
Bronivick <mister.galiakhmetov2016@yandex.ru>
2023-09-21 00:12:07 +0300

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = new int[4];
+
+int even = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 1000); 
+}
+
+Console.WriteLine(String.Join(", ", array));
+for (int j = 0; j< array.Length; j++)
+    if (array[j] % 2 == 0)
+        even++;
+
+Console.WriteLine(even);
+
